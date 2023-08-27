0699625a9ff0379ba633d05bb42953439d1d6952
Faris Mujcinagic <faris1306@hotmail.com>
2023-08-27 11:02:40 +0200

    About.js and Banner.js minor updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -69,7 +69,7 @@ export const Banner = () => {
                                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                     <span className="tagline">Welcome to my Portfolio</span>
                                     <h1>{`Hi! I'm Faris`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Second Year Computer Science Student", "Low", "3" ]'><span className="wrap">{text}</span></span></h1>
-                                    <p>Presently pursuing a <strong>Computer Science</strong> degree program at <strong>the University of Technology in Graz, Austria. </strong>
+                                    <p>Presently pursuing a <strong>Computer Science</strong> degree program at <strong>Graz University of Technology in Austria. </strong>
                                         I possess a strong interest in <strong>low-level programming</strong> and am highly motivated to apply my determination in excelling within an <strong>entry-level cybersecurity role.</strong> Furthermore, I thoroughly enjoy engaging in various forms of software development.</p>
                                     <p>Click on my resume below <span class="rotate-emoji">👨‍💻</span></p>
                                     <button onClick={handleClick} class="connect-button">Let's Connect <ArrowRightCircle size={25} /></button>
